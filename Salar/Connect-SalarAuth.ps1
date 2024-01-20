040ef96b03b031fe0e9f2620cf94009156503e9e
Sergio Carvaho Queiroz <queirozsc@gmail.com>
2024-01-20 12:09:23 -0400

    Updated code to get Salar workers with filters

---
+++ Salar/Connect-SalarAuth.ps1
@@ -0,0 +1,19 @@
+﻿#Authenticate to get the session token
+$SALAR_URI = 'https://clinicaamericas.salar10.net/api/Autenticacion/Autenticar'
+Write-Host "URI " $SALAR_URI
+
+$headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
+$headers.Add("Content-Type", "application/json")
+
+$postParam = @{
+    "IdEmpresa" = $env:SALAR_ID_EMPRESA
+    "Cuenta" = $env:SALAR_CUENTA
+    "Contrasena" = $env:SALAR_CONTRASENA
+}
+$body = ConvertTo-Json $postParam
+Write-Host "Body " $body
+
+$response = Invoke-RestMethod $SALAR_URI -Method 'POST' -Headers $headers -Body $body
+$response | ConvertTo-Json
+$env:SALAR_TOKEN = $response.Token
+Write-Host "Token " $env:SALAR_TOKEN
---
+++ Salar/Get-SalarWorkers.ps1
@@ -0,0 +1,24 @@
+﻿$SALAR_URI = 'https://clinicaamericas.salar10.net/api/Colaboradores/Consultar'
+Write-Host "URI " $SALAR_URI
+
+$SALAR_AUTHORIZATION = "Bearer " + $env:SALAR_TOKEN
+Write-Host "Authorization" $SALAR_AUTHORIZATION
+
+$headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
+$headers.Add("Content-Type", "application/json")
+$headers.Add("Authorization", $SALAR_AUTHORIZATION)
+$headers.Add("Cookie", "SALAR.CLINAME.SESION=1brhcycza3df2ymjmdblip4z")
+$postParam = @{
+    "IdEmpresa" = $env:SALAR_ID_EMPRESA
+    "Cuenta" = $env:SALAR_CUENTA
+    "Contrasena" = $env:SALAR_CONTRASENA
+    #"Estado" = 2 # Status of worker (1 = Active / 2 = Inactive)
+    "FechaIngresoInicio" = "2024-01-01" # Start date, in format yyyy-mm-dd
+    "FechaIngresoFin" = "2024-01-31" # End date, in format yyyy-mm-dd
+
+}
+$body = ConvertTo-Json $postParam
+Write-Host "Body " $body
+
+$response = Invoke-RestMethod $SALAR_URI -Method 'POST' -Headers $headers -Body $body
+$response | ConvertTo-Json
