566297b6b0d7f069569678ce9e2be3cd3828dc0d
Sergio Carvaho Queiroz <queirozsc@gmail.com>
2024-01-25 18:48:13 -0400

    Updated documentation for Filter-CustomObject function

--- Utils.ps1
+++ Utils.ps1
@@ -12,7 +12,6 @@
     Convert-Usernames -Fullname "Yersin Jacob Avalos Severiche" -Country "BOLIVIA" 
     # Returns [yavalos, yjavalos, yaavalos]
 #>
-
 Function Convert-Usernames {
     [CmdletBinding()]
     Param (
@@ -62,8 +61,6 @@ Function Convert-Usernames {
     Format-TitleCase "CLINICA METROPOLITANA DE LAS AMERICAS" 
     # Returns "Clinica Metropolitana de las Americas"
 #>
-
-
 function Format-TitleCase {
     [CmdletBinding()]
     param (
@@ -124,8 +121,6 @@ function Format-TitleCase {
     Join-FullName -FirstName SERGIO -FatherSurname CARVALHO -MotherSurname QUEIROZ
     #Returns "SERGIO CARVALHO QUEIROZ"
 #>
-
-
 function Join-Fullname {
     [CmdletBinding()]
     param (
@@ -157,5 +152,40 @@ function Join-Fullname {
     
     end {
         return $combinedString.Trim()
+    }
+}
+
+<#
+.SYNOPSIS
+    Returns a new custom object by filtering an existing object by any property
+.DESCRIPTION
+    Create a new custom object by filtering an existing object. You can use any property as filter criteria
+.EXAMPLE
+    $employees = Get-PayrollEmployees
+    $employee = Filter-CustomObject -OriginalObject $employees -FilterProperty CodigoColaborador -FilterValue 7737471
+    # Returns a single employee with ID equals to 7737471
+#>
+function Filter-CustomObject {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory=$True)]
+        [PSCustomObject] $OriginalObject,
+        [Parameter(Mandatory=$True)]
+        [string] $FilterProperty,  
+        [Parameter(Mandatory=$True)]
+        [string] $FilterValue  
+    )
+    
+    begin {
+        
+    }
+    
+    process {
+        # Filter the existing object based on the specified property and value
+        $FilteredObject = $originalObject | Where-Object { $_.$filterProperty -eq $filterValue }
+    }
+    
+    end {
+        return $FilteredObject
     }
 }
