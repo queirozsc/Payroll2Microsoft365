9be8b6134362cfc90fa003380dcbb51e2fd90bef
Sergio Carvaho Queiroz <queirozsc@gmail.com>
2024-01-25 20:09:18 -0400

    Created functions Get-ADUserByID and Get-ADUserByUsernames

---
+++ ActiveDirectory/Get-ADUserByID.ps1
@@ -0,0 +1,43 @@
+﻿<#
+.SYNOPSIS
+    Search for an Active Directory user by identification number
+.DESCRIPTION
+    Search for an user based on your identification number
+.NOTES
+    For integration purpose, the ID of employee was stored on Postal Code field of Active Directory
+.EXAMPLE
+    Get-ADUserByID 7737471
+    # Returns Active Directory user who has payroll code equals 7737471
+#>
+
+
+function Get-ADUserByID {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory=$True, ValueFromPipeline=$True)]
+        [string] $UserID
+    )    
+    begin {
+        
+    }
+    
+    process {
+        try {
+            # Get the user by identification document
+            Write-Verbose "Searching on $env:AD_SERVER_NAME for UserID $UserID ..."
+            $ADUser = Get-ADUser -Filter { PostalCode -eq $UserID}
+        }
+        catch {
+            Send-SentryEvent $_.Exception.Message
+        }
+        if ($ADUser) {
+            Write-Verbose "User $($ADUser.Name) has been found!"
+        } else {
+            Write-Verbose "User not found!"
+        }
+    }
+    
+    end {
+        return $ADUser
+    }
+}
---
+++ ActiveDirectory/Get-ADUserByUsernames.ps1
@@ -0,0 +1,63 @@
+﻿<#
+.SYNOPSIS
+    Search for an Active Directory user by a list of usernames
+.DESCRIPTION
+    Recursive search in Active Directory using a list of usernames
+.NOTES
+    To get a list of usernames from a full name, use the function Util\Convert-Usernames
+.EXAMPLE
+    $usernames = Convert-Usernames "SERGIO CARVALHO QUEIROZ"
+    $UnverifiedUser = Get-ADUserByUsernames $usernames
+    # Search AD, but not validating full name
+    $VerifiedUser = Get-ADUserByUsernames $usernames -VerificationName "SERGIO CARVALHO QUEIROZ"
+    # Search AD, verifying if the full name is the same of AD User
+#>
+
+function Get-ADUserByUsernames {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory=$True, ValueFromPipeline=$True)]
+        [array] $Usernames,
+        [Parameter(Mandatory=$False)]
+        [array] $VerificationName
+
+    )   
+    begin {
+        $UserFound = $False
+    }
+    
+    process {
+        # Check if the array is not empty
+        if ($Usernames -ne $null -and $Usernames.Count -gt 0) {
+            foreach ($Username in $Usernames) {
+                # Search Active Directory by account name
+                Write-Verbose "Searching on $env:AD_SERVER_NAME for Username $Username ..."
+                $ADUser = Get-ADUser -Filter { SamAccountName -eq $Username}
+
+                if ($ADUser) {
+                    # Double check user by full name
+                    if (-not [string]::IsNullOrEmpty($VerificationName)) {
+                        $UserFound = ($ADUser.Name.ToUpper() -eq $VerificationName.ToUpper())
+                    }
+                    else {
+                        # It's not necessary to check, then user was found!
+                        $UserFound = $True
+                    }
+                }
+                
+                # User was found! Break the loop
+                if ($UserFound) {
+                    Write-Verbose "User $($ADUser.Name) has been found!"
+                    break
+                }
+            }
+        }
+        else {
+            Write-Error "Usernames parameter is empty or null."
+        }
+    }
+    
+    end {
+        return $ADUser
+    }
+}
