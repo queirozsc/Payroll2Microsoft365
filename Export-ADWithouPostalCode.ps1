c60719249a5c0cbff8820457b5f33cc5dd7e477b
Sergio Carvaho Queiroz <queirozsc@gmail.com>
2024-02-01 15:28:45 -0400

    Ad hoc export data for revision

---
+++ Export-ADWithouPostalCode.ps1
@@ -0,0 +1,5 @@
+﻿$ADUsers = Get-ADUser -Filter "Enabled -eq '$true' -and (PostalCode -notlike '*')"
+$ADUsers `
+    | Select-Object Enabled, SamAccountName, Name, UserPrincipalName `
+    | Sort-Object SamAccountName `
+    | Export-Csv -Path ".\$(Get-Date -Format yyyyMMdd)_AD_Without_PostalCode.csv"
---
+++ Export-Salar-Retirados.ps1
@@ -0,0 +1,5 @@
+﻿$Employees = Get-PayrollEmployees -InactivesOnly
+$Employees `
+    | Select-Object Estado, FechaRetiro, CodigoColaborador, CorreoLaboral, FechaIngreso, CargoPlanillas, PrimerNombre, ApellidoPaterno, ApellidoMaterno  `
+    | Sort-Object FechaRetiro -Descending `
+    | Export-Csv -Path ".\$(Get-Date -Format yyyyMMdd)_Salar_Retirados.csv"
